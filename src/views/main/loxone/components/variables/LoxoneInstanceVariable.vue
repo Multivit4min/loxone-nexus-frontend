69ebeab1e0216c12421de364192bb489dd0b432b
David Kartnaller <david.kartnaller@gmail.com>
2025-09-19 21:31:11 +0000

    remove unused components

--- src/views/main/loxone/components/variables/LoxoneInstanceVariable.vue
+++ src/views/main/loxone/components/variables/LoxoneInstanceVariable.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import VariableToolbar from "./VariableToolbar.vue"
-import VariableListComponent from "./VariableListComponent.vue"
 import type { LoxoneInstance } from "@/api/types/loxone"
+import VariableList from "@/components/variables/VariableList.vue"
 
 const { instance } = defineProps<{ instance: LoxoneInstance }>()
 </script>
@@ -9,7 +9,7 @@ const { instance } = defineProps<{ instance: LoxoneInstance }>()
 <template>
   <div>
     <VariableToolbar :instance="instance" />
-    <VariableListComponent :instance="instance" />
+    <VariableList :instance="instance" />
   </div>
 </template>
 
