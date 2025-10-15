75367a72903fe41a9c62731ff247539ec3b1f042
David Kartnaller <david.kartnaller@gmail.com>
2025-10-15 14:18:49 +0000

    add script integration

--- src/integrations/script/help.ts
+++ src/integrations/script/help.ts
@@ -2,6 +2,11 @@ export const helpSnippet = `
 // Read the current value of a variable declared as an OUTPUT in the variable list
 nexus.get("some_output")
 
+//listen on event changes from inputs
+nexus.on("some_output", value => {
+  //value - represents the current value
+})
+
 // Set the value of a variable declared as an INPUT in the variable list
 nexus.set("some_input", 123)
 `.trim()
